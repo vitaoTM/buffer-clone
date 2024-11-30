b37fa080b9c6f6a8d828e90b6af2773088100fa6
vitor <vitor.grosskopf@gmail.com>
2024-11-30 17:09:12 -0300

    Add create action on controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,19 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfuly created an Account"
+    else
+      render :new # alert: "Something went wrong, try again"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
