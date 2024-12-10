8ef1c689942a5abd1ddc3e6e4206bd81e94b0649
vitor <vitor.grosskopf@gmail.com>
2024-12-10 17:23:33 -0300

    Enhance PasswordResetsController with edit and update actions

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,4 +10,27 @@ class PasswordResetsController < ApplicationController
     end
     redirect_to root_path, notice: "If your email was valid a token to reset your email was sent!"
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+      redirect_to sign_in_path, alert: "Your token has expied try again"
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Noice, Password reset was successful"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
