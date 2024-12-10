9dd0db038a25cdc8637aa7c97cf671d87bfea879
vitor <vitor.grosskopf@gmail.com>
2024-12-10 15:18:50 -0300

    Adds password validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+  validates :password, presence: true, length: 6..50
 end
