44d9aebbfec2c443adda4cab2e570cd453367baf
vitor <vitor.grosskopf@gmail.com>
2024-12-11 16:14:45 -0300

    Create Model TweeterAccount - Keep track of username, avatar, token and secret - Run: rails g model TweeterAccount user:belongs_to username image token secret - Run: rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_29_195422) do
+ActiveRecord::Schema.define(version: 2024_12_11_191141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "twitter_accounts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "username"
+    t.string "image"
+    t.string "token"
+    t.string "secret"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_twitter_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -22,4 +33,5 @@ ActiveRecord::Schema.define(version: 2024_11_29_195422) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "twitter_accounts", "users"
 end
