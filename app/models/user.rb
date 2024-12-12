3aac3ed0d38a2a8702bf5edc8df113b37b8b3edb
vitor <vitor.grosskopf@gmail.com>
2024-12-11 22:26:26 -0300

    Adds has_many association for Twitter accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 # password_digest:string
 
 class User < ApplicationRecord
+  has_many :twitter_accounts
+
   has_secure_password
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
   validates :password, presence: true, length: 6..50
