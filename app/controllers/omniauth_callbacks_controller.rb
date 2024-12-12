c1871c351dc2c4617582c80ba12b5c1f8f1783e0
vitor <vitor.grosskopf@gmail.com>
2024-12-11 22:28:02 -0300

    Create controller - Action: twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
-class OmniauthCallbvacksController < ApplicationController
+class OmniauthCallbacksController < ApplicationController
   def twitter
-    # render plain: "Success"
+    Rails.logger.info auth
+    Current.user.twitter_accounts.create(
+      name: auth.info.name,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret,
+    )
+
+    redirect_to root_path, notice: "Account connected Successfuly"
+  end
+
+  def auth
+    request.env("omniauth.auth")
   end
 end
