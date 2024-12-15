04c3ec0c90e86dae003400124078e1837a66295f
vitor <vitor.grosskopf@gmail.com>
2024-12-15 17:01:25 -0300

    User model: Add has_many association for Twitter accounts

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,21 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
     Rails.logger.info auth
-    Current.user.twitter_accounts.create(
-      name: auth.info.name,
+
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+
+    twitter_account.update(
+      # name: auth.info.username,
+      username: auth.info.nickname,
       image: auth.info.image,
       token: auth.credentials.token,
       secret: auth.credentials.secret,
     )
 
-    redirect_to root_path, notice: "Account connected Successfuly"
+    redirect_to twitter_accounts_path, notice: "Account connected Successfuly"
   end
 
   def auth
-    request.env("omniauth.auth")
+    request.env["omniauth.auth"]
   end
 end
