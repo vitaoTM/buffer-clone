6c2d8ef1449fc7c57d4e8ea5c9d926b239b47990
vitor <vitor.grosskopf@gmail.com>
2024-12-15 22:01:29 -0300

    Implement destroy action for twitter accounts controller

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -3,4 +3,10 @@ class TwitterAccountsController < ApplicationController
   def index
     @twitter_accounts = Current.user.twitter_accounts
   end
+
+  def destroy
+    @twitter_account = Current.user.twitter_accounts.find_by(params[:id])
+    @twitter_account.destroy
+    redirect_to twitter_accounts_path, notice: "Disconnected #{@twitter_account.username}"
+  end
 end
