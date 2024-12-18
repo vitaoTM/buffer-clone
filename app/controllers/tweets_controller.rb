e0ed447a2466eabfaee2852c88ffc9e46886239e
vitor <vitor.grosskopf@gmail.com>
2024-12-18 17:26:35 -0300

    TweetsController: Implement update action as part of the tweet scheduling feature

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_loged_in!
+  before_action :set_tweet, only: [:show, :destroy, :update]
 
   def index
     @tweets = Current.user.tweets
@@ -18,9 +19,29 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweets_params)
+      redirect_to tweets_path, notice: "Tweet was scheduled successfuly"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was unscheduled"
+  end
+
   private
 
   def tweets_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
